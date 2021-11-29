055f51e1824b72c193dc4e720eaf9d6155ee3e1b
Lukas Kubicek <kubicek.brno@gmail.com>
2021-11-29 13:58:47 +0100

    oprava 2.

--- SolutionApp/ConsoleApp1/Program.cs
+++ SolutionApp/ConsoleApp1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Nazdar lidi, buďte zdrávi");
-Console.WriteLine("naschledanou!");
+Console.WriteLine("Nashledanou!");
