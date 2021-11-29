3a7356052db79129c8b8a6360e731f5450333c8b
Jiří Mašek <jm.s@volny.cz>
2021-11-29 14:55:40 +0100

    třída pro culturu aplikace

---
+++ SolutionApp/ConsoleApp1/Appculture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+        public class AppCulture
+        {
+            public CultureInfo? VratCulturu(string jazyk)
+            {
+
+
+                try
+                {
+                    return new CultureInfo(jazyk);
+                }
+                catch (Exception e)
+                {
+                    return null;
+                }
+
+
+
+
+
+            }
+
+    
+}
