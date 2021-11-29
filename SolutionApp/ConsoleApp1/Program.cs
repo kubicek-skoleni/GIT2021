03f85c4ae30104cc7b5c1a770bbdc0e3259dd947
Martin Voral <fofafik@centrum.cz>
2021-11-29 14:41:01 +0100

    CZ pro cesky pozdrav jinak EN

--- SolutionApp/ConsoleApp1/Program.cs
+++ SolutionApp/ConsoleApp1/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Nazdar lidi, buďte zdrávi");
-Console.WriteLine("Nashledanou!");
+String lang= args.Length>0 ? args[0] : "CZ";
+
+if ("CZ".Equals(lang.ToUpper)) {
+    Console.WriteLine("Nazdar lidi, buďte zdrávi");
+    Console.WriteLine("Nashledanou!");
+}
+else
+{
+    Console.WriteLine("Hello, World");
+    Console.WriteLine("Goodby, World!");
+}
