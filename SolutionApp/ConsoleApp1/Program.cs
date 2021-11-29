63f3b61dca33ab5dd7ac29a821c105e69f749453
Lukas Kubicek <kubicek.brno@gmail.com>
2021-11-29 13:57:53 +0100

    hotfix: ukoncujiciho pozdravu

--- SolutionApp/ConsoleApp1/Program.cs
+++ SolutionApp/ConsoleApp1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Nazdar lidi, buďte zdrávi");
-Console.WriteLine("Goodbye, World!");
+Console.WriteLine("naschledanou!");
